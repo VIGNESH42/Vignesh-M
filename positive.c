894c378a23fda5438c8a420f24e5c36caf486e30
VIGNESH42 <35728153+VIGNESH42@users.noreply.github.com>
2018-01-25 16:34:47 +0530

    Create positive.c

---
+++ positive.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int a;
+  scanf("%d",&a);
+  if(a<0)
+  printf ("The value is negative\n");
+  else if(a>0)
+  printf ("The value is positive\n");
+  else
+  printf("The value is positive or negative");
+}
