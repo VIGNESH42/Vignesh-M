649b1acb9f3b32055936a41e26c8b777e69691f7
VIGNESH42 <35728153+VIGNESH42@users.noreply.github.com>
2018-02-06 16:47:28 +0530

    Create set1 7.c

---
+++ set1 7.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int b,i;
+    clrscr();
+    printf("Enter the N value:");
+    scanf("%d",&b);
+    for(i=1;i<=b;++i)
+    {
+        printf("Hello\n");
+    }
+    getch();
+}
