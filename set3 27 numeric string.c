f52307dd5e286109e5d1eb32c553226a47aad5d3
VIGNESH42 <35728153+VIGNESH42@users.noreply.github.com>
2018-03-01 16:34:07 +0530

    Create set3 27 numeric string.c

---
+++ set3 27 numeric string.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char a;
+printf("Enter the value of a");
+scanf("%d",&a);
+if(a>=0&&a<=9)
+{
+ printf("Yes");
+ }
+ else
+ {
+ printf("No");
+ }
+ getch();
+}
