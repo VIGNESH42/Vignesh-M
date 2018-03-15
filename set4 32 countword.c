8a20a9babcf1c609a5bf0ddef9a2e3beb468d585
VIGNESH42 <35728153+VIGNESH42@users.noreply.github.com>
2018-03-15 16:09:36 +0530

    Create set4 32 countword.c

---
+++ set4 32 countword.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int word,n=0;
+char str[100];
+printf("Enter the string\n");
+gets(str);
+for(word=0;str[word]!='\0';word++)
+{
+if(str[word]==' ' || str[word]=='.' || str[word]== ',')
+n++;
+}
+printf("The number of character in the sentences are %d\n",n+1);
+}
